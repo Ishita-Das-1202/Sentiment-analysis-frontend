fd1f342b8a80a5e7be581ed3f4c291cc09447170
Ishita Das <108291902+Ishita-Das-1202@users.noreply.github.com>
2025-02-18 14:51:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const BASE_URL = "https://server.aimliedc.in/ishita-das/sentiment-analysis"; // Change this to your backend URL if needed
+const BASE_URL = "https://server.aimliedc.tech/ishita-das/sentiment-analysis"; // Change this to your backend URL if needed
